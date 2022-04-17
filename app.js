4aa0ed55df899a215c9d3655803b88994ffd648a
rameem2003 <rameem2019@gmail.com>
2022-04-17 20:47:07 +0600

    option added

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var text = document.querySelector("textarea");
 var btn = document.querySelector("button");
+var select = document.querySelector("select");
 
+function voices(){
+    for(var voice of speechSynthesis.getVoices()){
+        console.log(voice);
+        var option = `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`;
+        select.insertAdjacentHTML("beforeend", option);
+    }
+}
+
+speechSynthesis.addEventListener("voiceschanged", voices)
 
 function text_to_speech(text){
-    var utternance = new SpeechSynthesisUtterance(text);
-    speechSynthesis.speak(utternance);
+    var speech = new SpeechSynthesisUtterance(text);
+    for(var voice of speechSynthesis.getVoices()){
+        if(voice.name === select.value){
+            speech.voice = voice;
+        }
+    }
+    speechSynthesis.speak(speech);
 }
 
 btn.addEventListener("click", (e) => {
